bc421e3ecfd1acad90a3f929008fdd3c7837f9f4
Elle Park <36606732+elle-park@users.noreply.github.com>
2020-05-18 20:41:23 -0400

    config for heroku

--- covid-smp/mysite/mysite/settings.py
+++ covid-smp/mysite/mysite/settings.py
@@ -236,3 +236,7 @@ THUMBNAIL_PROCESSORS = (
     'filer.thumbnail_processors.scale_and_crop_with_subject_location',
     'easy_thumbnails.processors.filters'
 )
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())

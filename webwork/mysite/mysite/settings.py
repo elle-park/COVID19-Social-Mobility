317895a6c54dc4a20a0c574be110b015cfb444fa
Elle Park <36606732+elle-park@users.noreply.github.com>
2020-05-20 15:21:52 -0400

    added procfile

--- webwork/mysite/mysite/settings.py
+++ webwork/mysite/mysite/settings.py
@@ -87,9 +87,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
-STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'mysite', 'static'),

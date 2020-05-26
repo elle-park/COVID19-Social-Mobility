ebcf69222876c254272dc68e89babeb6a3d43160
Elle Park <36606732+elle-park@users.noreply.github.com>
2020-05-26 11:58:39 -0400

    extract data from csv, change format using pandas

---
+++ SMChart/script.py
@@ -0,0 +1,22 @@
+import csv
+import pandas as pd
+
+# Import the SMIData.csv data: smi
+#smi = pd.read_csv('SMIData.csv')
+
+#colnames = ['date', 'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'NYC', 'OH', 'OK', 'OR', 'PA', 'PR', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'USA', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY']
+#data = pd.read_csv('SMIData.csv', names=colnames)
+
+data = pd.read_csv("SMIData.csv")
+dict_of_lists = {}
+
+# extract data's column as dict
+for col_name in data.columns:
+    temp = data[col_name].tolist()
+    dict_of_lists[col_name] = temp
+
+# write output to csv file with rows of col
+with open('output.csv', 'w', newline="") as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in dict_of_lists.items():
+       writer.writerow([key, value])

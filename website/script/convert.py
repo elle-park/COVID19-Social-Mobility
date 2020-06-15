c9c9c4b83516a06db1e298d199ccedd4f29d7754
Elle Park <36606732+elle-park@users.noreply.github.com>
2020-06-15 11:32:45 -0400

    updated website

---
+++ website/script/convert.py
@@ -0,0 +1,24 @@
+import csv
+import json
+import pandas as pd
+
+data = pd.read_csv("SMI-State-Data.csv")
+dict_of_lists = {}
+
+# extract data's column as dictionary
+for col_name in data.columns:
+    temp = data[col_name].tolist()
+    dict_of_lists[col_name] = temp
+
+# write output to csv file with rows of col
+with open('output.csv', 'w', newline="") as csv_file:
+    writer = csv.writer(csv_file)
+    for key, value in dict_of_lists.items():
+       writer.writerow([key, value])
+
+# Serializing JSON data
+json_object = json.dumps(dict_of_lists, indent = 4)
+
+# Writing to output.json
+with open("output.json", "w") as outfile:
+    outfile.write(json_object)

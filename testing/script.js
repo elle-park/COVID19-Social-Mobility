5d5cb631a142617c5e5f2d91c20ddc95e073a2ab
Elle Park <36606732+elle-park@users.noreply.github.com>
2020-06-30 18:18:26 -0400

    Update script.js

--- testing/script.js
+++ testing/script.js
@@ -70,8 +70,6 @@ chart.legend = new am4charts.Legend();
 
 // Add cursor
 chart.cursor = new am4charts.XYCursor();
-//chart.cursor.snapToSeries = alaska;
-//chart.cursor.xAxis = categoryAxis;
 
 // Add scroll bar - keep at bottom of graph
 chart.scrollbarX = new am4core.Scrollbar();
@@ -165,47 +163,31 @@ function sortTable(n) {
   switching = true;
   //Set the sorting direction to ascending:
   dir = "asc";
-  /*Make a loop that will continue until
-  no switching has been done:*/
+
   while (switching) {
-    //start by saying: no switching is done:
     switching = false;
     rows = table.rows;
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
     for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
       shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
       x = rows[i].getElementsByTagName("TD")[n];
       y = rows[i + 1].getElementsByTagName("TD")[n];
-      /*check if the two rows should switch place,
-      based on the direction, asc or desc:*/
       if (dir == "asc") {
         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
           shouldSwitch= true;
           break;
         }
       } else if (dir == "desc") {
         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
           shouldSwitch = true;
           break;
         }
       }
     }
     if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
       switching = true;
-      //Each time a switch is done, increase this count by 1:
       switchcount ++;
     } else {
-      /*If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again.*/
       if (switchcount == 0 && dir == "asc") {
         dir = "desc";
         switching = true;
@@ -239,8 +221,6 @@ function tableText(tableCell) {
   else {
     createSeries(location, location, "rectangle", false);
   }
-
-
 }
 
 // triangle - USA, regions

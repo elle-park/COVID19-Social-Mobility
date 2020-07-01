5150ee10eed152b31bccd1863507eae22682f1a8
Elle Park <36606732+elle-park@users.noreply.github.com>
2020-07-01 14:04:47 -0400

    updated data

--- data/combine.py
+++ data/combine.py
@@ -8,21 +8,24 @@ data_city = pd.read_csv("longitudinal-city-2020-06-22.csv")
 df = pd.DataFrame(data_state)
 df2 = pd.DataFrame(data_city)
 
-ne = ['ME', 'VT', 'NH', 'MA', 'RI', 'CT', 'NY', 'PA', 'NJ', 'DE']
+ne = ['ME', 'VT', 'NH', 'MA', 'RI', 'CT', 'NY', 'PA', 'NJ', 'DE', 'MD', 'DC']
 mw = ['MI', 'OH', 'IN', 'KY', 'MN', 'WI', 'IA', 'IL', 'MO']
-central = ['MT', 'ID', 'WY', 'ND', 'SD', 'NE', 'KS', 'OK', 'AR', 'MS', 'LA', 'TX', 'NM', 'CO', 'UT', 'NV']
-south = ['MD', 'DC', 'WV', 'VA', 'TN', 'NC', 'SC', 'GA', 'AL', 'FL']
+central = ['MT', 'ID', 'WY', 'ND', 'SD', 'NE', 'KS', 'OK', 'AR', 'MS', 'NM', 'CO', 'UT', 'NV']
+south = ['LA', 'TX', 'WV', 'VA', 'TN', 'NC', 'SC', 'GA', 'AL', 'FL']
 west = ['WA', 'OR', 'CA', 'AZ', 'AK', 'HI']
+carib = ['PR', 'VI']
 
 df['avg_USA'] = df.mean(axis=1)
-df['avg_Northeast'] = df[ne].mean(axis=1)
-df['avg_Midwest'] = df[mw].mean(axis=1)
-df['avg_Central'] = df[central].mean(axis=1)
-df['avg_South'] = df[south].mean(axis=1)
-df['avg_West'] = df[west].mean(axis=1)
+df['Northeast'] = df[ne].mean(axis=1)
+df['Midwest'] = df[mw].mean(axis=1)
+df['Central'] = df[central].mean(axis=1)
+df['South'] = df[south].mean(axis=1)
+df['West'] = df[west].mean(axis=1)
+df['Caribbean'] = df[carib].mean(axis=1)
 
 for col_name in df2.columns:
-    df[col_name] = df2[col_name]
+    df[col_name.replace("_", " ")] = df2[col_name]
 
 df.to_csv("output.csv", index=False)
-#print(df)
+#print(df.index)
+#df.iloc[0].transpose().to_csv("citylist.csv", index=True)

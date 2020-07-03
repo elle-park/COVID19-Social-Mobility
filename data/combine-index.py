78ab4ba44a7827d4a639b340003e0cc40ded4652
Elle Park <36606732+elle-park@users.noreply.github.com>
2020-07-02 20:04:12 -0400

    combine index

---
+++ data/combine-index.py
@@ -0,0 +1,11 @@
+import csv
+import numpy as np
+import pandas as pd
+
+data_state = pd.read_csv("index-state.csv")
+data_city = pd.read_csv("index-city.csv")
+
+df = data_state.append(data_city)
+
+#print(out)
+df.to_csv("index-total.csv", index=False)
